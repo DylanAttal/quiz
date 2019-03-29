5fa6dc3ec9592f04516d0eaac63a177a4788bf43
Dylan Attal <attal.dylan@gmail.com>
2019-03-29 16:34:16 -0400

    Jumbotron set up for QuestionBox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Question Box</h1>
+  <div>
+    <b-jumbotron>
+      <template slot="lead">Some Question Here</template>
+
+      <hr class="my-4">
+
+      <p>List of answers</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
