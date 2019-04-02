7cdeec0661d05433fd790726e0e6350c0ca94878
Dylan Attal <attal.dylan@gmail.com>
2019-04-02 15:10:55 -0400

    Adds lodash. Shuffles answers so that last one isn't always correct.

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,7 +6,12 @@
       <hr class="my-4">
 
       <b-list-group>
-        <b-list-group-item v-for="answer in answers" :key="answer">{{ answer }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
       <b-button variant="primary">Submit</b-button>
@@ -16,10 +21,18 @@
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   props: {
     currentQuestion: Object,
     next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      shuffledAnswers: []
+    };
   },
   computed: {
     answers() {
@@ -28,8 +41,53 @@ export default {
       return answers;
     }
   },
-  mounted() {
-    console.log(this.currentQuestion);
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+      }
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+    }
   }
 };
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+
+.btn {
+  margin: 0 5px;
+}
+
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.incorrect {
+  background-color: red;
+}
+</style>
